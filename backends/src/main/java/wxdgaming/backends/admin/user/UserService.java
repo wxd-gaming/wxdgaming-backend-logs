f1b3c60ff555004279bdfd932d4622373a7db756
wxd-gaming <492794628@qq.com>
2025-02-11 20:53:39 +0800

    【新增】用户服务类

---
+++ backends/src/main/java/wxdgaming/backends/admin/user/UserService.java
@@ -0,0 +1,16 @@
+package wxdgaming.backends.admin.user;
+
+import com.google.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 用户服务
+ *
+ * @author: wxd-gaming(無心道, 15388152619)
+ * @version: 2025-02-11 20:50
+ **/
+@Slf4j
+@Singleton
+public class UserService {
+
+}
