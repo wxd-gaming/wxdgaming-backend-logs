b107e941a176f4e5f6dee0b5eab87ae0506bc025
wxd-gaming <492794628@qq.com>
2025-07-24 13:39:03 +0800

    【优化】调试http协议的gzip压缩

--- backends/src/main/java/wxdgaming/backends/jsplugin/impl/JHttp.java
+++ backends/src/main/java/wxdgaming/backends/jsplugin/impl/JHttp.java
@@ -6,8 +6,8 @@ import wxdgaming.backends.admin.game.GameContext;
 import wxdgaming.backends.admin.game.GameService;
 import wxdgaming.boot2.core.util.Md5Util;
 import wxdgaming.boot2.starter.js.IJSPlugin;
-import wxdgaming.boot2.starter.net.httpclient5.GetRequest;
-import wxdgaming.boot2.starter.net.httpclient5.PostRequest;
+import wxdgaming.boot2.starter.net.httpclient5.HttpRequestGet;
+import wxdgaming.boot2.starter.net.httpclient5.HttpRequestPost;
 
 /**
  * 通关java http client 发送请求
@@ -30,19 +30,19 @@ public class JHttp implements IJSPlugin {
     }
 
     public String get(String url) {
-        return GetRequest.of(url).execute().bodyString();
+        return HttpRequestGet.of(url).execute().bodyString();
     }
 
     public String post(String url, String body) {
-        return PostRequest.of(url, body).execute().bodyString();
+        return HttpRequestPost.of(url, body).execute().bodyString();
     }
 
     public String postJson(String url, String json) {
-        return PostRequest.ofJson(url, json).execute().bodyString();
+        return HttpRequestPost.ofJson(url, json).execute().bodyString();
     }
 
     public String postMail(String url, String json) {
-        return PostRequest.ofJson(url, json).execute().bodyString();
+        return HttpRequestPost.ofJson(url, json).execute().bodyString();
     }
 
     public String md5(int gameId, String str) {
